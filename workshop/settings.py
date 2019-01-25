c33027661b5add202a8f937d3e6f3bd5c0c2a95c
saidjillo <umurjillo@gmail.com>
2019-01-26 02:13:40 +0300

    --

--- workshop/settings.py
+++ workshop/settings.py
@@ -101,18 +101,23 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gameworkshop',
-        'USER':'postgres',
-        'PASSWORD': 'benora90',
-        'HOST': 'localhost',
-        'PORT': '5432',
+ 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # DATABASES['default'] = dj_database_url.config()
 
 
+
+    #   'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'gameworkshop',
+    #     'USER':'postgres',
+    #     'PASSWORD': 'benora90',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 
 
 # Password validation
